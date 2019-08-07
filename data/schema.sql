6603a4fec2511e7c86c0e449fc94b9b4770ea5ca
Pomin Wu <pomin5@gmail.com>
2019-08-07 09:40:06 +0800

    Add SQL schema

--- data/schema.sql
+++ data/schema.sql
@@ -8,15 +8,8 @@ CREATE TABLE rates (
   year int not null,
   received_votes int not null,
   issued_votes int not null,
+  population int not null,
   rate real not null
-);
-
-CREATE TABLE dpp_to_kmt_rates (
-  city_county varchar(255) not null,
-  township_city varchar(255) not null,
-  village varchar(255) not null,
-  year int not null,
-  dpp_to_kmt_rate real not null
 );
 
 COPY rates FROM '/app/data/president_rates.csv' WITH (FORMAT csv);
